a1b2bdcfc2db35cac1360cc8c82514b30fb6c632
Caio Andrade <caiotdandrade@gmail.com>
2026-01-19 11:32:11 -0300

    moves files around so that bpr can be treated as a pip module¨

--- utils/tensor.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-
-def create_id_to_idx_lookup_tensor(tensor):
-    lookup = -1 + torch.zeros(tensor.max().item() + 1, dtype=torch.long)
-    lookup[tensor] = torch.arange(len(tensor))
-    return lookup
